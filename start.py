d3e9242a0f7b24af01249437b514e850cb0edef4
omnipotentnova <bradlangel@casketjane.local.tld>
2013-11-09 17:49:56 -0500

    automatic date

--- start.py
+++ start.py
@@ -4,7 +4,13 @@ Created on Nov 7, 2013
 @author: bradlangel
 '''
 from calendarrepository import *
+import datetime
 
-calendarRepo = CalendarRepository().GetEvents("2013-11-08", product)        
-print calendarRepo[0].summary
-print calendarRepo[1].summary
+
+calendarRepo = CalendarRepository().GetEvents(unicode(datetime.date.today()), product)        
+
+print len(calendarRepo)
+if len(calendarRepo):
+    print calendarRepo[0].summary
+    print calendarRepo[1].summary
+    
